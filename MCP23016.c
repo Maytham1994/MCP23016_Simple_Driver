4a077f33cf246a2b940103d277b7bbe51db62fdc
Maytham <maythamsabbagh@gmail.com>
2015-02-12 13:31:34 -0500

    Fixed Driver, Working Version

--- MCP23016.c
+++ MCP23016.c
@@ -1,8 +1,9 @@
 /*
 * MCP23016 16-Bit I/O Expander Library
 * Author: Maytham Sabbagh
-* Revision Date: February 10th, 2015
+* Revision Date: February 12th, 2015
 */
+
 #include <Wire.h>
 
 /* The A2,A1,A0 pins are connected to ground, therefore the address is 0x20
@@ -14,15 +15,23 @@
 #define GP1	0x01
 // When set to 1, the port is INPUT, when set to 0, the port is OUTPUT.
 #define IODIR0 0x06
-#define IODIR0 0x07
+#define IODIR1 0x07
 
 // Setting all the pins in a port to either output, or input
 #define ALL_OUTPUT	0x00
 #define ALL_INPUT	0xFF
 
-// These variables hold the current state of each of the two ports, directions of the I/O
-unsigned char GP0CurrentState;
-unsigned char GP1CurrentState;
+struct IOState{
+  // These variables hold the current state of each of the two ports, directions of the I/O
+  unsigned char GP0CurrentState;
+  unsigned char GP1CurrentState;
+};
+
+struct IOPortValues{
+  // These variables hold the current state of each of the two ports, directions of the I/O
+  unsigned char IOPort0;
+  unsigned char IOPort1;
+};
 
 // This function start the transmission I2C protocol
 void Start_I2C(){
@@ -34,10 +43,11 @@ void Start_I2C(){
 *	Parameters: GP0State and GP1State are the desired states for the two ports\
 *	This function updates the two current state parameters
 */
-void MCP_SetDirection(unsigned char GP0State, unsigned char GP1State){
-	// Update the two CurrentState parameters
-	GP0CurrentState = GP0State;
-	GP1CurrentState = GP1State;
+struct IOState MCP_SetDirection(unsigned char GP0State, unsigned char GP1State){
+	IOState CurrentState;
+        // Update the two CurrentState parameters
+	CurrentState.GP0CurrentState = GP0State;
+	CurrentState.GP1CurrentState = GP1State;
 	// Start I2C transmission with the device
 	Wire.beginTransmission(IOAddress);
 	// Selecting IODIR0 register to write to
@@ -50,6 +60,8 @@ void MCP_SetDirection(unsigned char GP0State, unsigned char GP1State){
 	Wire.write(GP1State);
 	// Ending I2C transmission
 	Wire.endTransmission();
+
+        return CurrentState;
 }
 
 /*
@@ -80,14 +92,40 @@ void MCP_WritePort(bool port, unsigned char data){
 }
 
 /*
+*	This function reads the data from ports 0 and 1 and returns an array with the data
+*	Parameters: port (true for port1, false for port0)
+*	Return: array with data from port0 and port1
+*/
+struct IOPortValues MCP_ReadPorts(bool port){
+        IOPortValues result;
+	// Temporary array to hold he values
+	unsigned char array[2];
+	// Temporarily save 0 in both values
+	array[0]=0x00;
+	array[1]=0x00;
+	// Start transmission with the device
+	Wire.beginTransmission(IOAddress);
+	// Request the device for the number of bytes, if port1 then the bytes are 2, if port0 then the bytes are 1
+	Wire.requestFrom(IOAddress, port+1);
+	// Save the data in the array
+	for(int i = 0; Wire.available(); i++){
+		array[i] = Wire.read();
+	}
+        result.IOPort0 = array[0];
+        result.IOPort1 = array[1];
+	// Return the array
+	return result;
+}
+
+/*
 *	This function is specific to the H-Bridge used for Group Hotel in ELEC3907 Project
 *	Parameters: AIN1,AIN2,BIN1,BIN2 are specific for the H-Bridge
 */
 void Motor_Directions(bool AIN1, bool AIN2, bool BIN1, bool BIN2){
 	// Read the current values in the port
-	unsigned char array[] = MCP_ReadPorts(0);
+	IOPortValues values = MCP_ReadPorts(0);
 	// Save the port in a temporary direction variable
-	unsigned char direction = array[0];
+	unsigned char directions = values.IOPort0;
 	// Temporary Direction Variables
 	unsigned char temp1 = 0x0F;
 	unsigned char temp2 = 0x00;
@@ -105,9 +143,9 @@ void Motor_Directions(bool AIN1, bool AIN2, bool BIN1, bool BIN2){
 	temp2 |= BIN2 << 4;
 	
 	// ANDing the current values with temp1
-	direction &= temp1;
+	directions &= temp1;
 	// ORing the current values with temp2
-	direction |= temp2;
+	directions |= temp2;
 	
 	/*
 	*	Code above does this;
@@ -118,28 +156,5 @@ void Motor_Directions(bool AIN1, bool AIN2, bool BIN1, bool BIN2){
 	*	Therefore direction is now 0b11001010, therefore only the 4 bits changed and the rest remained the same
 	*/
 	// Call the write function to write the data to port0
-	MCP_WritePort(0, direction);
-}
-
-/*
-*	This function reads the data from ports 0 and 1 and returns an array with the data
-*	Parameters: port (true for port1, false for port0)
-*	Return: array with data from port0 and port1
-*/
-unsigned char[] MCP_ReadPorts(bool port){
-	// Temporary array to hold he values
-	unsigned char array[2];
-	// Temporarily save 0 in both values
-	array[0]=0x00;
-	array[1]=0x00;
-	// Start transmission with the device
-	Wire.beginTransmission(IOAddress);
-	// Request the device for the number of bytes, if port1 then the bytes are 2, if port0 then the bytes are 1
-	Wire.requestFrom(IOAddress, port+1);
-	// Save the data in the array
-	for(int i = 0; Wire.available(); i++){
-		array[i] = Wire.read();
-	}
-	// Return the array
-	return array;
+	MCP_WritePort(0, directions);
 }
